ffc339fd87321f41865b6bd42fb0cda4782c686a
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-02-25 02:39:13 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -15,3 +15,14 @@ l_range=int(input("Enter the lower range:"))
 u_range=int(input("Enter the upper range:"))
 a=[(x,x**2) for x in range(l_range,u_range+1)]
 print(a)
+
+def arr(arr , l):
+  for i in l:
+    if i%2==0
+      return even
+  else:
+    return odd
+    
+    arr=[2,33,23,43,55,343]
+    l=len(arr)
+  print(arr(arr,l))
